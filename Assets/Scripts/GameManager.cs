781a4ea19ccb6887c52b8b227f71dd342d04f17a
neilconnatty <neil.connatty@gmail.com>
2016-06-15 16:57:03 -0700

    changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.Networking;
 public class GameManager : NetworkBehaviour
 {
     public static GameManager gm;
+    public static NetworkManager nm;
     public string levelAfterVictory;
     public GameObject humptyDumptyPieceOne;
 
@@ -12,6 +13,9 @@ public class GameManager : NetworkBehaviour
     {
         if (gm == null)
             gm = this.GetComponent<GameManager> ();
+
+        if (nm == null)
+            nm = GameObject.FindWithTag("NetworkManager").GetComponent<NetworkManager> ();
 
         humptyDumptyPieceOne.SetActive(false);
     }
@@ -23,6 +27,12 @@ public class GameManager : NetworkBehaviour
 
     public void PickUpPiece ()
     {
-        Debug.Log ("receive click");
+        StartCoroutine (LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel ()
+    {
+        yield return new WaitForSeconds (0.5f);
+        nm.ServerChangeScene (levelAfterVictory);
     }
 }
