4db3da80bf625e0dcb0d890291db38f74adff73f
neilconnatty <neil.connatty@gmail.com>
2016-06-15 16:58:22 -0700

    changes pt. 2

--- Assets/Scripts/MagicWord.cs
+++ Assets/Scripts/MagicWord.cs
@@ -6,26 +6,26 @@ public class MagicWord : MonoBehaviour
 {
 	public CrosswordInputField[] magicLetters;
 
-	//private Color[] _previousColors;
+	private Color[] _previousColors;
 
 	void Start ()
 	{
-		//_previousColors = new Color[magicLetters.Length];
+		_previousColors = new Color[magicLetters.Length];
 	}
 
 	public void lightSwtichOn ()
 	{
 		for (int i = 0; i < magicLetters.Length; i++) {
-			//_previousColors[i] = magicLetters[i].getColor ();
+			_previousColors[i] = magicLetters[i].getColor ();
 			magicLetters[i].setColor (Color.yellow);
 		}
 	}
-	/*
+
 	public void lightSwtichOff ()
 	{
 		for (int i = 0; i < magicLetters.Length; i++) {
 			magicLetters[i].setColor (_previousColors[i]);
 		}
 	}
-	*/
+
 }
