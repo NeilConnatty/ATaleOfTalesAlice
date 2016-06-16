8396f21013778e7426a416c9b5cfd17ce0a7f49e
neilconnatty <neil.connatty@gmail.com>
2016-06-16 11:37:53 -0700

    Blacklight working

--- Assets/Scripts/LightSwitch.cs
+++ Assets/Scripts/LightSwitch.cs
@@ -1,13 +1,37 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
 [RequireComponent (typeof (Collider))]
-public class LightSwitch : MonoBehaviour
+[RequireComponent (typeof (Material))]
+public class LightSwitch : NetworkBehaviour
 {
     public MagicWord magicWord;
 
+    private Renderer _rend;
+
+    [SyncVar(hook="StateChange")] bool blackLightOn = false;
+
+    void Awake ()
+    {
+        _rend = this.GetComponent<Renderer> ();
+    }
+
     void OnMouseDown ()
     {
-        magicWord.lightSwtichOn ();
+        blackLightOn = !blackLightOn;
+    }
+
+    void StateChange (bool newState)
+    {
+        blackLightOn = newState;
+
+        if (blackLightOn) {
+            _rend.material.SetColor("_Color", Color.red);
+            magicWord.lightSwtichOn ();
+        } else {
+            _rend.material.SetColor("_Color", Color.green);
+            magicWord.lightSwtichOff ();
+        }
     }
 }
