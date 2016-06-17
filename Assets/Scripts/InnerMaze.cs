a7a470164b0ee4d18aa0da194ed35b6b9225d34a
neilconnatty <neil.connatty@gmail.com>
2016-06-17 16:16:18 -0700

    implemented new maze

---
+++ Assets/Scripts/InnerMaze.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InnerMaze : MonoBehaviour {
+
+	private bool _initialized = false;
+
+	// Hack to initialize innerMaze y position
+	void Update () {
+		//if (_initialized) return;
+		Vector3 tempPosition = this.GetComponent<Transform> ().position;
+        tempPosition.y = 7;
+        this.GetComponent<Transform> ().position = tempPosition;
+	}
+
+
+}
