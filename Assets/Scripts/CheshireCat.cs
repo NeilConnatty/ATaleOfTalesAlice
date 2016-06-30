8e17e2e6f693c31200ff8da000a8af3ac76ea021
neilconnatty <neil.connatty@gmail.com>
2016-06-30 16:24:19 -0700

    implemented some changes for puzzle 2

--- Assets/Scripts/CheshireCat.cs
+++ Assets/Scripts/CheshireCat.cs
@@ -5,14 +5,17 @@ using System.Collections;
 public class CheshireCat : MonoBehaviour
 {
     public CardLock cardLock;
+    public TextMesh catTextOne;
+    public TextMesh catTextTwo;
 
     void OnMouseDown ()
     {
         if (cardLock.checkCorrectness ()) {
-            Debug.Log ("correct!");
+            catTextOne.text = "Thank you, you've\nput them in line.";
+            catTextTwo.text = "Now go fix this\nfriend of mine.";
             GameManager.gm.SolvePuzzle ();
         } else {
-            Debug.Log ("not correct");
+            catTextOne.text = "That isn't right.\nThey're still not\nin line.";
         }
     }
 }
