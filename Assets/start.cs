dea127290cba3f039f26033a0fb6ad0af19b9d92
apeksha <apekshadarbari@gmail.com>
2016-07-09 12:10:00 -0700

    Prototype for P1 networking integration in VR.

---
+++ Assets/start.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class start : MonoBehaviour {
+
+	public void clickBtn (string name) {
+
+		switch (name) {
+
+		case "Start":
+
+			Application.LoadLevel ("Choose_Game");
+			break;
+
+
+
+		case "Quit":
+			Application.Quit ();
+			break;
+		}
+
+	}
+
+		public void chooseServer (string name) {
+
+		//Code for choosing server goes here. Variable "name" can be used as server name. At the end, loads the main scene.
+
+		Application.LoadLevel ("Maze");
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
