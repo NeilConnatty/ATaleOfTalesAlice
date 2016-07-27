1527436d59cfccfececf89ce11ff25ee6ec5ec9c
neilconnatty <neil.connatty@gmail.com>
2016-07-27 11:31:26 -0700

    added humpty

---
+++ Assets/Scripts/HumptyManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System.Collections;
+
+public class HumptyManager : MonoBehaviour
+{
+    public GameObject pieceOne;
+    public GameObject pieceTwo;
+    public GameObject wholeHumpty;
+
+    void Start ()
+    {
+        pieceOne.SetActive (false);
+        pieceTwo.SetActive (false);
+        wholeHumpty.SetActive (false);
+        InitializePieces ();
+    }
+
+    void InitializePieces ()
+    {
+        State gameState = GameState.gs.getState ();
+        switch (gameState) {
+            case State.PUZZLE1 :
+                pieceOne.SetActive (true);
+                break;
+
+            case State.MAZE2 :
+                pieceOne.SetActive (true);
+                break;
+
+            case State.PUZZLE2 :
+                pieceOne.SetActive (true);
+                pieceTwo.SetActive (true);
+                break;
+
+            case State.MAZE3 :
+                pieceOne.SetActive (true);
+                pieceTwo.SetActive (true);
+                break;
+
+            case State.PUZZLE3 :
+                wholeHumpty.SetActive (true);
+                break;
+
+            case State.MAZE4 :
+                wholeHumpty.SetActive (true);
+                break;
+        }
+    }
+}
