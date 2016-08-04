f8e67a2139b11ba152c03ee11cd09e4620555beb
neilconnatty <neil.connatty@gmail.com>
2016-08-03 17:23:31 -0700

    added cursor

---
+++ Assets/Scripts/CustomCursor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Script to allow game to have custom cursor
+ */
+public class CustomCursor : MonoBehaviour
+{
+	public Texture2D cursor;
+	public int cursorSizeX = 20;
+	public int cursorSizeY = 20;
+
+	void OnGUI ()
+	{
+		Cursor.SetCursor (cursor, Vector2.zero, CursorMode.ForceSoftware);
+	}
+}
